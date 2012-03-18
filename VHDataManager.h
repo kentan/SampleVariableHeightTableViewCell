5c93bfe54d8f7df6217690480dc5dd6af04c10a8
Kenta SHIMADA <hyperkentakun@gmail.com>
2012-03-19 02:27:10 +0900

    first commit

---
+++ VHDataManager.h
@@ -0,0 +1,13 @@
+//
+//  VHTDataManager.h
+//  SampleVariableHeightTableViewCell
+//
+//  Created by  on 12/03/11.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface VHTDataManager : NSObject
+
+@end
---
+++ VHDataManager.m
@@ -0,0 +1,13 @@
+//
+//  VHTDataManager.m
+//  SampleVariableHeightTableViewCell
+//
+//  Created by  on 12/03/11.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "VHTDataManager.h"
+
+@implementation VHTDataManager
+
+@end
